1593879d79311d111e6e3b7158655dcfcdf0bf21
Waynexchan <162703158+Waynexchan@users.noreply.github.com>
2024-03-12 09:15:43 +0000

    Transform it as a function

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ picture = [
   [0,0,0,1,0,0,0]
 ]
 
-fill = "*"
-empty = " "
-for row in picture:
-  for pixel in row:
-    if (pixel): #Pixel contains 0 / 1 only
-      print(fill, end ="")
-    else:
-      print(empty, end="")
-  print('')
+def show_tree():
+  fill = "*"
+  empty = " "
+  for row in picture:
+    for pixel in row:
+      if (pixel): #Pixel contains 0 / 1 only
+        print(fill, end ="")
+      else:
+        print(empty, end="")
+    print('')
 
+show_tree()
